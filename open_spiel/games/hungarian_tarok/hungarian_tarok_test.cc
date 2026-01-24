12a52ea89d7b177ef75290f2e9abdd97ba10222c
Turu Tamás <tamas@turu.hu>
2026-01-24 13:38:05 +0100

    remove leduc-specific code from hungarian tarok

--- open_spiel/games/hungarian_tarok/hungarian_tarok_test.cc
+++ open_spiel/games/hungarian_tarok/hungarian_tarok_test.cc
@@ -33,61 +33,12 @@ void BasicHungariantarokTests() {
   testing::LoadGameTest("hungarian_tarok");
   testing::ChanceOutcomesTest(*LoadGame("hungarian_tarok"));
   testing::RandomSimTest(*LoadGame("hungarian_tarok"), 100);
-  testing::RandomSimTest(*LoadGame("hungarian_tarok",
-                         {{"action_mapping", GameParameter(true)}}), 100);
-  testing::RandomSimTest(*LoadGame("hungarian_tarok",
-                         {{"suit_isomorphism", GameParameter(true)}}), 100);
-  for (Player players = 3; players <= 5; players++) {
-    testing::RandomSimTest(
-        *LoadGame("hungarian_tarok", {{"players", GameParameter(players)}}), 100);
-  }
-  testing::ResampleInfostateTest(*LoadGame("hungarian_tarok"), /*num_sims=*/100);
+  testing::RandomSimTest(*LoadGame("hungarian_tarok", {{"players", GameParameter(2)}}), 100);
+  testing::RandomSimTest(*LoadGame("hungarian_tarok", {{"players", GameParameter(4)}}), 100);
   auto observer = LoadGame("hungarian_tarok")
                       ->MakeObserver(kDefaultObsType,
                                      GameParametersFromString("single_tensor"));
   testing::RandomSimTestCustomObserver(*LoadGame("hungarian_tarok"), observer);
-}
-
-void PolicyTest() {
-  using PolicyGenerator = std::function<TabularPolicy(const Game& game)>;
-  std::vector<PolicyGenerator> policy_generators = {
-      GetAlwaysFoldPolicy,
-      GetAlwaysCallPolicy,
-      GetAlwaysRaisePolicy
-  };
-
-  std::shared_ptr<const Game> game = LoadGame("hungarian_tarok");
-  for (const auto& policy_generator : policy_generators) {
-    testing::TestEveryInfostateInPolicy(policy_generator, *game);
-    testing::TestPoliciesCanPlay(policy_generator, *game);
-  }
-}
-
-void StartingPlayerTest() {
-  std::shared_ptr<const Game> game =
-      LoadGame("hungarian_tarok", {{"players", GameParameter(3)},
-                               {"starting_player", GameParameter(1)}});
-  std::unique_ptr<State> state = game->NewInitialState();
-  SPIEL_CHECK_TRUE(state->IsChanceNode());
-  // In 3-player Leduc, deck is J,Q,K,A of 2 suits.
-  // J=0,1; Q=2,3; K=4,5; A=6,7
-  state->ApplyAction(0);  // P0 gets J
-  state->ApplyAction(2);  // P1 gets Q
-  state->ApplyAction(4);  // P2 gets K
-
-  // Round 1 betting. Starting player is 1.
-  SPIEL_CHECK_EQ(state->CurrentPlayer(), 1);
-  state->ApplyAction(ActionType::kFold);   // P1 folds.
-  SPIEL_CHECK_EQ(state->CurrentPlayer(), 2);
-  state->ApplyAction(ActionType::kRaise);  // P2 raises.
-  SPIEL_CHECK_EQ(state->CurrentPlayer(), 0);
-  state->ApplyAction(ActionType::kCall);   // P0 calls.
-
-  // Round 2, deal public card.
-  SPIEL_CHECK_TRUE(state->IsChanceNode());
-  state->ApplyAction(3);  // Public card is a Q.
-  // Player 1 folded, so player 2 should be next.
-  SPIEL_CHECK_EQ(state->CurrentPlayer(), 2);
 }
 
 }  // namespace
@@ -96,6 +47,4 @@ void StartingPlayerTest() {
 
 int main(int argc, char** argv) {
   open_spiel::hungarian_tarok::BasicHungariantarokTests();
-  open_spiel::hungarian_tarok::PolicyTest();
-  open_spiel::hungarian_tarok::StartingPlayerTest();
 }
