b48dc335d565a6c6a7a527b8951820a255e2611d
Turu Tamás <tamas@turu.hu>
2026-01-29 01:32:08 +0100

    add forgotten file card.cc

---
+++ open_spiel/games/hungarian_tarok/card.cc
@@ -0,0 +1,74 @@
+#include "card.h"
+
+namespace open_spiel {
+namespace hungarian_tarok {
+    std::ostream& operator<<(std::ostream& os, const Suit& suit) {
+        switch (suit) {
+            case Suit::kHearts: os << "Hearts"; break;
+            case Suit::kDiamonds: os << "Diamonds"; break;
+            case Suit::kClubs: os << "Clubs"; break;
+            case Suit::kSpades: os << "Spades"; break;
+            case Suit::kTarok: os << "Tarok"; break;
+        }
+        return os;
+    }
+    std::ostream& operator<<(std::ostream& os, const SuitRank& rank) {
+        switch (rank) {
+            case SuitRank::kAceTen: os << "Ace/Ten"; break;
+            case SuitRank::kJack: os << "Jack"; break;
+            case SuitRank::kRider: os << "Rider"; break;
+            case SuitRank::kQueen: os << "Queen"; break;
+            case SuitRank::kKing: os << "King"; break;
+        }
+        return os;
+    }
+    bool CardBeats(Card a, Card b) {
+        SPIEL_CHECK_LT(a, kDeckSize);
+        SPIEL_CHECK_LT(b, kDeckSize);
+        
+        Suit suit_a = CardSuit(a);
+        Suit suit_b = CardSuit(b);
+        if (suit_a == Suit::kTarok && suit_b != Suit::kTarok) {
+            return true;
+        } else if (suit_a != Suit::kTarok && suit_b == Suit::kTarok) {
+            return false;
+        } else if (suit_a == Suit::kTarok && suit_b == Suit::kTarok) {
+            return a > b; // higher tarok wins
+        } else {
+            // both are suit cards
+            if (suit_a != suit_b) {
+                return false; // different suits, first does not win
+            } else {
+                return a > b; // higher rank wins
+            }
+        }
+    }
+    std::string CardToString(Card card) {
+        SPIEL_CHECK_GE(card, 0);
+        SPIEL_CHECK_LT(card, kDeckSize);
+        if (card < kNumTaroks) {
+            return absl::StrCat("Tarok ", card + 1);
+        } else {
+            Suit suit = CardSuit(card);
+            SuitRank rank = static_cast<SuitRank>((card - kNumTaroks) % kNumRanksPerSuit);
+            return absl::StrCat(suit, " ", rank);
+        }
+    }
+    std::string DeckToString(const Deck &deck) {
+        std::stringstream ss;
+        for (Player p = 0; p < kNumPlayers; ++p) {
+            int count = 0;
+            ss << "Player " << p << " hand: ";
+            for (Card card = 0; card < kDeckSize; ++card) {
+                if (deck[card] == p) {
+                    count++;
+                    ss << CardToString(card) << "; ";
+                }
+            }
+            ss << " (total " << count << ")";
+            ss << std::endl;
+        }
+        return ss.str();
+    }
+} // namespace hungarian_tarok
+} // namespace open_spiel
